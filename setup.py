6d670282f21b2721db7db6b536ff0b5073475463
krypton-byte <rosid6434@gmail.com>
2021-10-20 23:35:20 +0700

    .

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   name = 'tiktok_downloader',        
   packages = ['tiktok_downloader'],
   include_package_data=True, 
-  version = '0.1.5',    
+  version = '0.1.6',    
   license='MIT',     
   description = 'Tiktok Downloader&Scraper using bs4&requests', 
   author = 'Krypton Byte',                  
