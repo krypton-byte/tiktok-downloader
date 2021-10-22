a531e9502baad5a3f3dd327ab53a8878b170b809
krypton-byte <rosid6434@gmail.com>
2021-10-22 11:30:16 +0700

    gh

--- resetup.py
+++ resetup.py
@@ -4,7 +4,7 @@ arg=argparse.ArgumentParser()
 arg.add_argument('--version',type=str)
 args=arg.parse_args()
 if args.version:
-    new=open('setup.py').read().replace('0.1.6',re.search(r'\/?([0-9\.]+)',args.version).group(1))
+    new=open('setup.py').read().replace('0.1.6',re.search(r'\/?([0-9A-Za-z\.]+)',args.version).group(1))
     with open('setup.py','w') as fil:
         print(new)
         fil.write(new)
